d82413a02e1679a2061c59855612dfbb3e1bd366
leelasd <leela.dodda.yale@gmail.com>
2024-08-27 16:35:12 -0400

    chore: Update S4 model parameters for demonstration purposes

--- scripts/all_together.py
+++ scripts/all_together.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
 
     # Create an S4 model with (almost) the same parameters as in the paper.
     s4 = S4forDenovoDesign(
-        n_max_epochs=3,  # This is for only demonstration purposes. Set this to a (much) higher value for actual training. Default: 400.
-        batch_size=64,  # This is for only demonstration purposes. The value in the paper is 2048.
+        n_max_epochs=400,  # This is for only demonstration purposes. Set this to a (much) higher value for actual training. Default: 400.
+        batch_size=2048,  # This is for only demonstration purposes. The value in the paper is 2048.
         device="cuda",  # replace this with "cpu" if you don't have a CUDA-enabled GPU.
     )
     # Pretrain the model on a small subset of ChEMBL
